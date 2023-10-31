9373e0cfcaacf45416a115a4d9398e8b9f1c9d4d
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-31 19:59:44 +0000

    Add participants link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dig_participants
+  resources :dig_participants, only: [:new, :create, :destroy]
   resources :digs
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
