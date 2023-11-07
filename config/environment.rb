41d69e5d0465211624ec69526c1c8f5d1fb1e171
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-11-07 16:26:39 +0000

    Fixed dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 require "carrierwave"
-require "carrierwae/orm/activerecord"
+require "carrierwave/orm/activerecord"
 
 # Initialize the Rails application.
 Rails.application.initialize!
