bc1804edeaf3a674a54d46969ab3873fddfea180
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-11-07 00:21:39 +0000

    Added errors for user not found

--- app/controllers/digs_controller.rb
+++ app/controllers/digs_controller.rb
@@ -22,19 +22,24 @@ class DigsController < ApplicationController
   # POST /digs or /digs.json
   def create
     primary_investigator = User.find_by(email: dig_params[:primary_investigator_email])
-    new_params = dig_params.except(:primary_investigator_email)
-    new_params[:primary_investigator_id] = primary_investigator.id
-    @dig = Dig.new(new_params)
 
     respond_to do |format|
-      if @dig.save
-        format.html { redirect_to dig_url(@dig), notice: "Dig was successfully created." }
-        format.json { render :show, status: :created, location: @dig }
-        DigParticipant.create(dig_id: @dig.id, participant: current_user, role: "admin")
-        DigParticipant.create(dig_id: @dig.id, participant: primary_investigator, role: "primary investigator" )
+      if primary_investigator
+        new_params = dig_params.except(:primary_investigator_email)
+        new_params[:primary_investigator_id] = primary_investigator.id
+        @dig = Dig.new(new_params)
+        if @dig.save
+          format.html { redirect_to dig_url(@dig), notice: "Dig was successfully created." }
+          format.json { render :show, status: :created, location: @dig }
+          DigParticipant.create(dig_id: @dig.id, participant: current_user, role: "admin")
+          DigParticipant.create(dig_id: @dig.id, participant: primary_investigator, role: "primary investigator" )
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @dig.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @dig.errors, status: :unprocessable_entity }
+        format.html { redirect_to new_dig_url, status: :unprocessable_entity, notice: "User not found."}
+        format.json { render json: { error: "User not found" }, status: :unprocessable_entity }
       end
     end
   end
