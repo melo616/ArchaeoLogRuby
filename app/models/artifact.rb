032b96cc62244a8e6a7e3909f2a9c916f72e3659
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-11-07 17:33:21 +0000

    Run migrations and install erd gem

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,5 +1,22 @@
+# == Schema Information
+#
+# Table name: artifacts
+#
+#  id            :integer          not null, primary key
+#  description   :text
+#  lat           :float
+#  lng           :float
+#  material      :string
+#  mohs_hardness :decimal(, )
+#  site          :string
+#  weight        :float
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  dig_id        :integer
+#  poster_id     :integer
+#
 class Artifact < ApplicationRecord
-  belongs_to: dig
-  belongs_to: user
+  belongs_to :dig
+  belongs_to :user
   
 end
