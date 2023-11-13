3d4877a36aad698297673758c9475b384f98987d
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-11-13 22:36:12 +0000

    Colors

--- app/policies/dig_policy.rb
+++ app/policies/dig_policy.rb
@@ -15,7 +15,25 @@ class DigPolicy < ApplicationPolicy
     @dig.users.include?(user)
   end
 
+  def edit?
+    update?
+  end
+
+  def update?
+    lead?
+  end
+
+  def destroy?
+    lead?
+  end
+
   def can_see_date?
     # is_dig_leader?
+  end
+
+  private
+
+  def lead?
+    dig.leads.any? { |lead| lead.participant_id == user.id }
   end
 end
