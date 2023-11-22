62e6ac8df803c5d07d309520349464b28755b6ef
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-11-22 23:33:25 +0000

    Invitations can't be sent to existing participants

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -18,20 +18,27 @@ class InvitationsController < ApplicationController
     invitation_params = params.require(:invitation).permit(:dig_id, :participant_email, :role)
     participant_email = invitation_params[:participant_email]
     participant = User.find_by(email: participant_email)
+    dig_id = invitation_params[:dig_id]
+    dig = Dig.find(dig_id)
     respond_to do |format|
       if participant
-        sender = current_user
-        @invitation = Invitation.new(dig_id: invitation_params[:dig_id], recipient: participant, sender: sender, role: invitation_params[:role])
-          if @invitation.save
-            format.html { redirect_to root_path, notice: "Invitation was successfully sent." }
-            format.json { render :show, status: :created, location: @invitation }
-          else
-            format.html { render :new, status: :unprocessable_entity }
-            format.json { render json: @invitation.errors, status: :unprocessable_entity }
-          end
+        if dig.dig_participants.any? { |dig_participant| dig_participant.participant_id == participant.id}
+          format.html { redirect_to dig_dig_participants_path(dig), status: :unprocessable_entity, notice: "User is already a participant of this dig."}
+          format.json { render json: { error: "Participant already exists." }, status: :unprocessable_entity }
         else
-          format.html { redirect_to root_path, status: :unprocessable_entity, notice: "User not found. Please verify user is registered with ArchaeoLog."}
-          format.json { render json: { error: "User not found" }, status: :unprocessable_entity }
+          sender = current_user
+          @invitation = Invitation.new(dig_id: invitation_params[:dig_id], recipient: participant, sender: sender, role: invitation_params[:role])
+            if @invitation.save
+              format.html { redirect_to root_path, notice: "Invitation was successfully sent." }
+              format.json { render :show, status: :created, location: @invitation }
+            else
+              format.html { render :new, status: :unprocessable_entity }
+              format.json { render json: @invitation.errors, status: :unprocessable_entity }
+            end
+        end
+      else
+        format.html { redirect_to root_path, status: :unprocessable_entity, notice: "User not found. Please verify user is registered with ArchaeoLog."}
+        format.json { render json: { error: "User not found" }, status: :unprocessable_entity }
       end
     end
   end
